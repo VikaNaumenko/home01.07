1ba19a8307aead1c0024ff96dc81a417ffadd07d
VikaNaumenko <67087076+VikaNaumenko@users.noreply.github.com>
2023-12-29 20:18:09 +0100

    add proect

---
+++ Home01.07/Home01.07/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Home01._07.MvcApplication" Language="C#" %>
---
+++ Home01.07/Home01.07/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Home01._07.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public int Price {  get; set; }
+        public int PriceOld { get; set;}
+    }
+}
